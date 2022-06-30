03f55aeed81332568e4887fb3bf1aa7ebf828bda
Rustam Kashapov <hardtechnik@yandex.ru>
2022-06-30 16:14:23 +0300

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,21 @@
+from unittest import mock
+
 import pytest
+from mopidy.models import Album as MopidyAlbum, Artist as MopidyArtist, Track as MopidyTrack
+from yandex_music import (
+    Album,
+    AlbumEvent,
+    Artist,
+    ArtistEvent,
+    Cover,
+    Day,
+    DownloadInfo,
+    Event,
+    Feed,
+    Search,
+    SearchResult,
+    Track,
+)
 
 from mopidy_ymusic.backend import YMusicBackend
 
@@ -9,5 +26,140 @@ def config():
 
 
 @pytest.fixture
-def backend(config):
-    return YMusicBackend(config, None)
+def artist():
+    return Artist(id='artist-0001', name='Artist 01 Name', cover=Cover(uri='images.com/artist/cover.jpg?size=%%'))
+
+
+@pytest.fixture
+def album(artist):
+    return Album(
+        id='album-0001',
+        title='Album 01 Title',
+        artists=[artist],
+        cover_uri='images.com/album/cover.jpg?size=%%',
+    )
+
+
+@pytest.fixture
+def track(album, artist):
+    return Track(
+        id='track-0001',
+        title='Track 01 Title',
+        artists=[artist],
+        albums=[album],
+        duration_ms=100500,
+        cover_uri='images.com/track/cover.jpg?size=%%',
+    )
+
+
+@pytest.fixture
+def mopidy_artist(artist):
+    return MopidyArtist(uri=f'ymusic:artist:{artist.id}', name=artist.name, sortname=artist.name)
+
+
+@pytest.fixture
+def mopidy_album(mopidy_artist, album):
+    return MopidyAlbum(uri=f'ymusic:album:{album.id}', name=album.title, artists=[mopidy_artist])
+
+
+@pytest.fixture
+def mopidy_track(mopidy_album, mopidy_artist, track):
+    return MopidyTrack(
+        uri=f'ymusic:track:{track.id}',
+        name=track.title,
+        length=track.duration_ms,
+        album=mopidy_album,
+        artists=[mopidy_artist],
+    )
+
+
+@pytest.fixture
+def client(album, artist, track):
+    client = mock.Mock()
+    client.feed.return_value = Feed(
+        days=[
+            Day(
+                day='2022.06.30',
+                tracks_to_play_with_ads=[],
+                tracks_to_play=[],
+                events=[
+                    Event(
+                        id='event-0001',
+                        type='albums',
+                        title='Album Event',
+                        albums=[AlbumEvent(album=album, tracks=[])],
+                        artists=[],
+                        tracks=[],
+                    ),
+                    Event(
+                        id='event-0002',
+                        type='artists',
+                        title='Artist Event',
+                        albums=[],
+                        artists=[ArtistEvent(
+                            artist=artist,
+                            tracks=[],
+                            similar_to_artists_from_history=[],
+                            subscribed=False,
+                        )],
+                        tracks=[],
+                    ),
+                ],
+            ),
+        ],
+        can_get_more_events=False,
+        pumpkin=False,
+        is_wizard_passed=False,
+        generated_playlists=False,
+        headlines=[],
+        today='2022-06-30',
+    )
+
+    album.volumes = [[track]]
+    client.albums_with_tracks.return_value = album
+    client.artists_tracks.return_value = [track]
+    client.tracks.return_value = [track]
+    client.artists.return_value = [artist]
+    client.albums.return_value = [album]
+    client.search.return_value = Search(
+        search_request_id='',
+        text='',
+        best=None,
+        playlists=None,
+        videos=None,
+        users=None,
+        podcasts=None,
+        podcast_episodes=None,
+        albums=SearchResult(results=[album], type='album', per_page=1, order=0, total=1),
+        artists=SearchResult(results=[artist], type='artist', per_page=1, order=0, total=1),
+        tracks=SearchResult(results=[track], type='track', per_page=1, order=0, total=1),
+    )
+    infos = [
+        DownloadInfo(codec='aac', bitrate_in_kbps=128, gain=False, preview=False, download_info_url='', direct=True),
+        DownloadInfo(codec='aac', bitrate_in_kbps=192, gain=False, preview=False, download_info_url='', direct=True),
+        DownloadInfo(codec='mp3', bitrate_in_kbps=128, gain=False, preview=False, download_info_url='', direct=True),
+        DownloadInfo(codec='mp3', bitrate_in_kbps=320, gain=False, preview=False, download_info_url='', direct=True),
+    ]
+
+    for info in infos:
+        info.direct_link = f'https://server.com/track/{info.bitrate_in_kbps}/{info.codec}'
+
+    client.tracks_download_info.return_value = infos
+    return client
+
+
+@pytest.fixture
+def backend(config, client):
+    backend = YMusicBackend(config, None)
+    backend.client = client
+    return backend
+
+
+@pytest.fixture
+def library(backend):
+    return backend.library
+
+
+@pytest.fixture
+def playback(backend):
+    return backend.playback
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,29 +1,79 @@
-from mopidy.models import Ref
+from mopidy.models import Image, Ref
 
 
-def test_backend(backend):
-    seen = set()
+def assert_refs_equal(one, two):
+    assert one.type == two.type
+    assert one.uri == two.uri
+    assert one.name == two.name
 
-    def walk(ref):
-        assert ref.uri not in seen
-        seen.add(ref.uri)
-        uris = []
-        refs = backend.library.browse(ref.uri)[:5]
 
-        if ref.type != Ref.DIRECTORY:
-            assert backend.library.lookup(ref.uri) != []
+def test_library_root(library):
+    assert_refs_equal(library.root_directory, Ref.directory(uri='ymusic:directory:root', name='Yandex Music'))
 
-        if ref.type == Ref.TRACK:
-            assert backend.playback.translate_uri(ref.uri)
 
-        assert backend.library.get_images([ref.uri])
+def test_browse(library, album, track, artist):
+    album_event_ref, artist_event_ref = library.browse(library.root_directory.uri)
+    assert_refs_equal(album_event_ref, Ref.directory(uri='ymusic:directory:event-0001', name='Album Event'))
+    assert_refs_equal(artist_event_ref, Ref.directory(uri='ymusic:directory:event-0002', name='Artist Event'))
 
-        for ref in refs:
-            uris.append(ref.uri)
-            uris.extend(walk(ref))
+    [album_ref] = library.browse(album_event_ref.uri)
+    assert_refs_equal(album_ref, Ref.album(uri=f'ymusic:album:{album.id}', name=album.title))
 
-        return uris
+    [track_ref] = library.browse(album_ref.uri)
+    assert_refs_equal(track_ref, Ref.track(uri=f'ymusic:track:{track.id}', name=track.title))
 
-    assert walk(backend.library.root_directory) != []
-    search_result = backend.library.search({'any': ['Beatles - Yellow Submarine']}, [])
-    assert len(search_result.tracks) > 0
+    [artist_ref] = library.browse(artist_event_ref.uri)
+    assert_refs_equal(artist_ref, Ref.artist(uri=f'ymusic:artist:{artist.id}', name=artist.name))
+
+    [track_ref] = library.browse(artist_ref.uri)
+    assert_refs_equal(track_ref, Ref.track(uri=f'ymusic:track:{track.id}', name=track.title))
+
+
+def test_lookup(library, mopidy_album, mopidy_artist, mopidy_track):
+    assert library.lookup(library.root_directory.uri) == []
+    assert library.lookup(mopidy_artist.uri) == [mopidy_track]
+    assert library.lookup(mopidy_album.uri) == [mopidy_track]
+    assert library.lookup(mopidy_track.uri) == [mopidy_track]
+
+
+def test_search(library, client, mopidy_album, mopidy_artist, mopidy_track):
+    def assert_result(result):
+        assert result.uri == 'ymusic:search'
+        assert result.albums == (mopidy_album,)
+        assert result.artists == (mopidy_artist,)
+        assert result.tracks == (mopidy_track,)
+
+    result = library.search({'any': ['AnyText']}, [])
+    client.search.assert_called_with('AnyText', type_='all')
+    assert_result(result)
+
+    result = library.search({'artist': ['Artist']}, [])
+    client.search.assert_called_with('Artist', type_='artist')
+    assert_result(result)
+
+    result = library.search({'album': ['Album']}, [])
+    client.search.assert_called_with('Album', type_='album')
+    assert_result(result)
+
+    result = library.search({'track': ['Track']}, [])
+    client.search.assert_called_with('Track', type_='track')
+    assert_result(result)
+
+    client.search.reset_mock()
+    result = library.search({'foobar': ['Unexpected']}, [])
+    client.search.assert_not_called()
+    assert result.albums == ()
+    assert result.tracks == ()
+    assert result.artists == ()
+
+
+def test_get_images(library, mopidy_artist, mopidy_album, mopidy_track):
+    images = library.get_images([mopidy_artist.uri, mopidy_album.uri, mopidy_track.uri])
+    assert images[mopidy_artist.uri] == [Image(uri='https://images.com/artist/cover.jpg?size=400x400')]
+    assert images[mopidy_album.uri] == [Image(uri='https://images.com/album/cover.jpg?size=400x400')]
+    assert images[mopidy_track.uri] == [Image(uri='https://images.com/track/cover.jpg?size=400x400')]
+
+
+def test_playback(playback, client, track, mopidy_track):
+    assert playback.translate_uri(mopidy_track.uri) == 'https://server.com/track/128/mp3'
+    client.tracks_download_info.assert_called_with(track.id, get_direct_links=True)
